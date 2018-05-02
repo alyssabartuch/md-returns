400d1e66670efe41e15a8822d89ae9bcfb9ad56d
alyssa_bartuch <alyssabartuch@gmail.com>
2018-05-02 12:59:06 -0400

    logic and styling push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -108,9 +108,132 @@ export class AppComponent {
                   denialReason: ''
               }
           ]
+      },
+      {
+          id: 100000076,
+          returnDate: 'Apr 19, 2018',
+          returnStatus: 'Completed',
+          items: [{
+              itemName: 'Digitizer for iPad 2 - White',
+              sku: 'MDAP0541',
+              orderNo: 100058475,
+              returnReason: 'Backlight - Falling Off',
+              comments: '',
+              status: 'approved',
+              creditMemo: '#100005764',
+              denialReason: ''
+          }]
+      },
+      {
+          id: 100000037,
+          returnDate: 'Apr 6, 2018',
+          returnStatus: 'Completed',
+          items: [
+              {
+                  itemName: 'Digitizer for iPad 2 - Black',
+                  sku: 'MDAP0021',
+                  orderNo: 100051906,
+                  returnReason: 'Backlight - Falling Off',
+                  comments: '',
+                  status: 'approved',
+                  creditMemo: '#100005954',
+                  denialReason: ''
+              },
+              {
+                  itemName: 'Glass Lens for iPhone 4 (GSM & CDMA) - White',
+                  sku: 'DAP0412',
+                  orderNo: 100051897,
+                  returnReason: 'Backlight - Falling Off',
+                  comments: '',
+                  status: 'denied',
+                  creditMemo: '',
+                  denialReason: 'Denied - Heavy scratches on Digitizer'
+              }
+          ]
+      },
+      {
+          id: 100000764,
+          returnDate: 'Mar 21, 2018',
+          returnStatus: 'Completed',
+          items: [{
+              itemName: 'Digitizer for iPhone 4 - White',
+              sku: 'MDAP6541',
+              orderNo: 100076475,
+              returnReason: 'Backlight - Falling Off',
+              comments: '',
+              status: 'approved',
+              creditMemo: '#100008764',
+              denialReason: ''
+          }]
+      },
+      {
+          id: 100000753,
+          returnDate: 'Feb 16, 2018',
+          returnStatus: 'Completed',
+          items: [{
+              itemName: 'Digitizer for iPad Pro - Black',
+              sku: 'MDAP6575',
+              orderNo: 100076465,
+              returnReason: 'Backlight - Falling Off',
+              comments: '',
+              status: 'denied',
+              creditMemo: '',
+              denialReason: 'Denied - Heavy scratches on Digitizer'
+          }]
+      },
+      {
+          id: 100000678,
+          returnDate: 'Jan 29, 2018',
+          returnStatus: 'Completed',
+          items: [
+              {
+                  itemName: 'Digitizer for iPad 2 - White',
+                  sku: 'MDAP0054',
+                  orderNo: 100051986,
+                  returnReason: 'Backlight - Falling Off',
+                  comments: '',
+                  status: 'denied',
+                  creditMemo: '',
+                  denialReason: 'Denied - Heavy scratches on Digitizer'
+              },
+              {
+                  itemName: 'Glass Lens for iPhone 4 (GSM & CDMA) - Black',
+                  sku: 'DAP0412',
+                  orderNo: 100051897,
+                  returnReason: 'Backlight - Falling Off',
+                  comments: '',
+                  status: 'approved',
+                  creditMemo: '#100005424',
+                  denialReason: ''
+              }
+          ]
       }
 
   ];
+
+
+  shouldDisplayCreditMemo(returnItem) {
+      var displayCreditMemo = false;
+
+      if (returnItem.status == "approved") {
+          displayCreditMemo = true;
+      }
+
+
+      return displayCreditMemo;
+
+  }
+
+  shouldDisplayDenialReason(returnItem) {
+      var displayDenialReason = false;
+
+      if (returnItem.status == "denied") {
+          displayDenialReason = true;
+      }
+
+      return displayDenialReason;
+
+  }
 
 
 
